9619a01e98e851de96fb64e69c516ed611f8eb17
IvanK <kyselovivan@gmail.com>
2018-08-14 00:24:56 +0300

    added separated db's depends on dev/test/remote

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 let db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
+    localhost: process.env.MONGODB_URI,
     mlab: 'mongodb://admin:a12345@ds219832.mlab.com:19832/tododb'
   };
   
