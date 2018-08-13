3ea051fc86d1a74beb6ca643c9ca8edfab2c5d1f
IvanK <kyselovivan@gmail.com>
2018-08-13 14:42:11 +0300

    fix before heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect("mongodb://<admin>:<a12345>@ds219832.mlab.com:19832/tododb" || "mongodb://localhost:27017/TodoApp");
 
 module.exports = { mongoose }; 
