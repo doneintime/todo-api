768033f7da8f337fbe81bb25c3c2e2747dcf22ee
IvanK <kyselovivan@gmail.com>
2018-08-14 17:20:12 +0300

    added user model with tokens

--- server/server.js
+++ server/server.js
@@ -27,13 +27,14 @@ app.post('/todos', (req, res)=>{
 })
 
 app.post('/users', (req,res)=>{
-    var user = new User({
-        email: "kyselovivan@gmail.com"
-    })
-    user.save().then((usr)=>{
-        res.send(usr);
-    }, (e)=>{
-        res.status(400).send(e);
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+    user.save().then(()=>{
+        return user.generateAuthToken();
+    }).then((token)=>{
+        res.header('x-auth', token).send(user);
+    }).catch((e)=>{
+        res.status(400).send();
     })
 })
 
